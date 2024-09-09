4845dde78a232302a589e8d3c9d4f8c60b6c626a
rudraksh5312 <singhalrudraksh9@gmail.com>
2024-09-09 08:06:49 +0530

    adding files

---
+++ numbersystem/negative.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main (){
+    long long int n ;
+    cin>>n;
+
+    unsigned long long int i = 0; ans = 0;
+
+    while (n < 0){
+         
+        n = pow(2,16)+n;
+    }
+
+         cout<< n << endl;
+         while (n){
+            int lastbit = n & 1;
+            ans = (pow(10,i )* lastbit) + ans;
+            n = n>> 1;
+            i++;
+            cout << ans << endl;
+         }
+         cout << ans << endl;
+         return 0 ;
+ }
+
+   
