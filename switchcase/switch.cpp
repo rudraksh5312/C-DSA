7c33581f0d7d2e4e9823e8f4aade12a1c05de370
rudraksh5312 <singhalrudraksh9@gmail.com>
2024-09-09 08:08:00 +0530

    adding files

---
+++ switchcase/switch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    char ch = '1';
+
+    cout<< endl;
+    switch( ch ){
+
+        case 1: cout << "First"<< endl;
+                break;
+        case '1': cout<< "character"<< endl;
+                break;
+
+        default: cout << "It is default case " <<  endl;    
+    }
+    cout << endl;
+    return 0 ;
+} 
